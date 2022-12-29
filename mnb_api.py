203c1066f8be390ad08727012ec67a093c0ee73e
petycomppety <30414255+petycomppety@users.noreply.github.com>
2022-12-29 15:06:49 +0100

    added 404 error handler

--- mnb_api.py
+++ mnb_api.py
@@ -1,20 +1,30 @@
 from mnb import MNBClient, XMLParser
 from flask import Flask, request
+from flask_restful import Resource, Api, abort
 import json
 
 app = Flask(__name__)
+api = Api(app)
+
+class ExchangeRateResource(Resource):
+    def get(self):
+        # Make the request to the MNB web service
+        mnb_client = MNBClient()
+        xml_parser = XMLParser()
+        result = mnb_client.get_exchange_rates()
+        if result is None:
+            # Return a 404 error if the result is None
+            abort(404, message="Exchange rates not found")
+        currencies = xml_parser.process_xml(result)
+        # Return the exchange rate as a JSON response
+        return {'MBN Exchange Rates': currencies}, 200
 
 
-@app.route('/exchange_rate', methods=['GET'])
-def get_exchange_rate():
+api.add_resource(ExchangeRateResource, '/exchange_rate')
 
-    # Make the request to the MNB web service
-    mnb_client = MNBClient()
-    xml_parser = XMLParser()
-    result = mnb_client.get_exchange_rates()
-    currencies = xml_parser.process_xml(result)
-    # Return the exchange rate as a JSON response
-    return json.dumps(currencies)
+@app.errorhandler(404)
+def handle_404_error(error):
+    return {'error': 'Not found'}, 404
 
 if __name__ == '__main__':
     app.run()
